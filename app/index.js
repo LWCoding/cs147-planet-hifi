44283b6b2009ae5c04d4fa9dee1d7bfb96c57042
LWCoding <DiamondMob01@gmail.com>
2024-11-20 03:51:03 -0800

    Added note about index.js (will later become onboarding)

--- app/index.js
+++ app/index.js
@@ -5,25 +5,30 @@ import { Link } from "expo-router";
 // Import components
 import Login from "@/components/onboarding/Login";
 
+/*
+NOTE FROM LUCAS:
+Instead of login screen this will eventually be onboarding
+*/
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Login />
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Login />
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  link: {
-    color: "blue",
-    textDecorationLine: "underline",
-    marginTop: 20,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	link: {
+		color: "blue",
+		textDecorationLine: "underline",
+		marginTop: 20,
+	},
 });
