2d68ab99e6b05846726556bc37e9befa1c4e2b2b
LWCoding <DiamondMob01@gmail.com>
2024-11-20 20:32:10 -0800

    Galaxy now fetches all planets from database and renders statuses

--- app/tabs/galaxy/index.js
+++ app/tabs/galaxy/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, FlatList } from "react-native";
 import { Button } from "react-native-paper";
@@ -5,22 +6,61 @@ import { Button } from "react-native-paper";
 // Import components
 import Planet from "@/components/Planet";
 
+// Import database access
+import db from "@/database/db";
+
 export default function Galaxy() {
-  const planets = [
-    { username: "Anya", emotion: "happy" },
-    { username: "Jeff", emotion: "angry" },
-    { username: "Bezos", emotion: "neutral" },
-    { username: "Lucas", emotion: "sad" },
-    { username: "Carolyn", emotion: "happy" },
-    { username: "Evelyn", emotion: "angry" },
-    { username: "Kristine", emotion: "neutral" },
-    { username: "Gray", emotion: "sad" },
-  ];
+  const [planets, setPlanets] = useState(null);
+
+  // Fetch all planets from the database and return them in the form
+  // [{username: String, emotion: String}]
+  const fetchPlanets = async () => {
+    const { data: usersData, error: usersError } = await db
+      .from("users")
+      .select("*");
+
+    if (usersError) {
+      console.error("Error fetching users: " + error.message);
+    }
+
+    const { data: statusData, error: statusError } = await db
+      .from("statuses")
+      .select("*");
+
+    if (statusError) {
+      console.error("Error fetching statuses: " + error.message);
+    }
+
+    const usersInfo = usersData.map((user) => {
+      // If the user has a status, then find the status
+      let emotion = "happy"; // Default emotion to happy
+      if (user.current_status_id) {
+        const status = statusData.find(
+          (status) => status.user_id === user.user_id
+        );
+        emotion = status.emotion;
+      }
+
+      return {
+        username: user.username,
+        emotion,
+      };
+    });
+
+    console.log(usersInfo);
+
+    setPlanets(usersInfo);
+  };
+
+  // Get all planets from the database
+  useEffect(() => {
+    fetchPlanets();
+  }, []);
 
   return (
     <View style={styles.container}>
       {/* This is an example of how to use a React Native Paper element - Lucas */}
-      <Button mode="contained" onPress={() => console.log("Button Pressed!")}>
+      <Button mode="contained" onPress={() => fetchPlanets()}>
         Press Me
       </Button>
 
