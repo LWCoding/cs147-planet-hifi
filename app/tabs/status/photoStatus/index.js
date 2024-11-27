8456fb89c87ad0c98faaf45b908a61c25a99b8fe
LWCoding <DiamondMob01@gmail.com>
2024-11-27 02:50:09 -0800

    Photo status now shows preview of image after taking picture

--- app/tabs/status/photoStatus/index.js
+++ app/tabs/status/photoStatus/index.js
@@ -1,11 +1,14 @@
-import { CameraView, useCameraPermissions } from "expo-camera";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { Camera, CameraView, useCameraPermissions } from "expo-camera";
+import { useRef, useState } from "react";
+import { StyleSheet, TouchableOpacity, View, Image } from "react-native";
 import { Button, Text } from "react-native-paper";
 
 import { useTheme } from "react-native-paper";
 
 export default function PhotoStatus() {
   const theme = useTheme();
+  const cameraRef = useRef(null);
+  const [picUri, setPicUri] = useState("");
   const [cameraPerms, requestCameraPerms] = useCameraPermissions();
 
   // If this is null, camera permissions are still loading
@@ -25,18 +28,36 @@ export default function PhotoStatus() {
     );
   }
 
+  const takePicture = async () => {
+    console.log("Taking picture...");
+    const pic = await cameraRef.current.takePictureAsync();
+    console.log(pic.uri); // Saves the URI of the picture
+    setPicUri(pic.uri);
+  };
+
   // Or else, load the camera background and the photo button
   return (
     <View
       style={[styles.container, { backgroundColor: theme.colors.background }]}
     >
-      <CameraView style={styles.camera} facing="back">
+      <CameraView style={styles.camera} ref={cameraRef} facing="back">
         <TouchableOpacity
+          onPress={takePicture}
           style={[
             styles.cameraButton,
             { backgroundColor: theme.colors.onBackground },
           ]}
         ></TouchableOpacity>
+        <Image // This is a placeholder for now! We should open on a separate screen
+          style={{
+            position: "absolute",
+            right: 20,
+            bottom: 50,
+            width: 100,
+            height: 100,
+          }}
+          source={{ uri: picUri }}
+        ></Image>
       </CameraView>
     </View>
   );
