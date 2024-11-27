59916e5402869a1fa58020b437fe4d10d92ada1e
LWCoding <DiamondMob01@gmail.com>
2024-11-27 02:17:21 -0800

    Added expo-camera library and created picture background for photo status screen

--- app/tabs/status/photoStatus.js
+++ app/tabs/status/photoStatus.js
@@ -1,16 +1,43 @@
-import { StyleSheet, View } from "react-native";
-import { Text } from "react-native-paper";
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { Button, Text } from "react-native-paper";
 
 import { useTheme } from "react-native-paper";
 
 export default function PhotoStatus() {
   const theme = useTheme();
+  const [cameraPerms, requestCameraPerms] = useCameraPermissions();
 
+  // If this is null, camera permissions are still loading
+  if (!cameraPerms) {
+    return <View />;
+  }
+
+  // If no camera permissions were granted, ask for permission
+  if (!cameraPerms.granted) {
+    return (
+      <View
+        style={[styles.container, { backgroundColor: theme.colors.background }]}
+      >
+        <Text>Camera permissions are required to take a photo</Text>
+        <Button onPress={requestCameraPerms}>Request Perms</Button>
+      </View>
+    );
+  }
+
+  // Or else, load the camera background and the photo button
   return (
     <View
       style={[styles.container, { backgroundColor: theme.colors.background }]}
     >
-      <Text>Photo!!!</Text>
+      <CameraView style={styles.camera} facing="back">
+        <TouchableOpacity
+          style={[
+            styles.cameraButton,
+            { backgroundColor: theme.colors.onBackground },
+          ]}
+        ></TouchableOpacity>
+      </CameraView>
     </View>
   );
 }
@@ -20,5 +47,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  cameraButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    top: "85%", // How far down the camera button is
+    left: "50%",
+    transform: [{ translateX: "-50%" }, { translateY: "-50%" }], // Necessary to center camera button
   },
 });
