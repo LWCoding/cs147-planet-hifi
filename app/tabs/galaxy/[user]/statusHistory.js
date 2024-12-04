8a409911947debf42dbe52675066b9a83afa0d56
LWCoding <DiamondMob01@gmail.com>
2024-12-04 14:58:16 -0800

    Posts are listed in history screen (still not stylized to look good)

--- app/tabs/galaxy/[user]/statusHistory.js
+++ app/tabs/galaxy/[user]/statusHistory.js
@@ -1,27 +1,37 @@
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import { ActivityIndicator, Text } from "react-native-paper";
 import { useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
 import { useTheme } from "react-native-paper";
-import { findPlanetById } from "@/database/db";
+import {
+  fetchAllStatusesById,
+  findPlanetById,
+  findStatusById,
+} from "@/database/db";
+
+// Import components
+import StatusHistoryBlock from "@/components/StatusHistoryBlock";
 
 export default function statusHistory() {
   const { user: inputtedUserId } = useLocalSearchParams(); // Get the user's info from navigation
 
   const theme = useTheme();
   const [user, setUser] = useState(null);
+  const [statuses, setStatuses] = useState(null);
 
   const fetchUserInfo = async () => {
     const user = await findPlanetById(inputtedUserId);
+    const statuses = await fetchAllStatusesById(user.user_id);
 
     setUser(user);
+    setStatuses(statuses);
   };
 
   useEffect(() => {
     fetchUserInfo();
   }, []);
 
-  if (!user) {
+  if (!user || !statuses) {
     return (
       <View
         style={[styles.container, { backgroundColor: theme.colors.background }]}
@@ -38,6 +48,15 @@ export default function statusHistory() {
       style={[styles.container, { backgroundColor: theme.colors.background }]}
     >
       <Text style={styles.text}>{user.first_name}'s Post History</Text>
+      <FlatList
+        style={styles.statusesList}
+        data={statuses}
+        contentContainerStyle={{ alignItems: "center" }}
+        keyExtractor={(item) => item.status_id.toString()}
+        renderItem={({ item }) => (
+          <StatusHistoryBlock status={item} user={user} />
+        )}
+      />
     </View>
   );
 }
@@ -72,40 +91,8 @@ const styles = StyleSheet.create({
     fontFamily: "PPPierSans-Regular",
     fontSize: 15,
   },
-  expandButton: {
-    position: "absolute",
-    right: 40,
-    bottom: "32%",
-  },
-  buttonRow: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
+  statusesList: {
     width: "100%",
-    margin: 15,
-    paddingVertical: 10,
-    paddingHorizontal: 40,
-  },
-  circle: {
-    width: 50, // Circle's diameter
-    height: 50, // Circle's diameter
-    borderRadius: 25, // Half of the width/height for a perfect circle
-    justifyContent: "center", // Center the icon vertically
-    alignItems: "center", // Center the icon horizontally
-  },
-  planetContainer: {
-    padding: 30,
-    justifyContent: "center",
-    fontFamily: "PPPierSans-Regular",
-    alignItems: "center",
-    position: "absolute",
-    top: "7%",
-    zIndex: 1,
-  },
-  image: {
-    width: "80%",
-    height: "50%",
-    marginTop: 30,
-    borderRadius: 15,
   },
   statusContainer: {
     margin: 3,
