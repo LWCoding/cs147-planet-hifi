74a803f5ddd232e20dd1f68c56a2ff71db97ff19
LWCoding <DiamondMob01@gmail.com>
2024-12-05 15:35:17 -0800

    More styling

--- app/tabs/galaxy/newGalaxy/_layout.js
+++ app/tabs/galaxy/newGalaxy/_layout.js
@@ -4,62 +4,62 @@ import IonIcon from "@expo/vector-icons/Ionicons";
 import { useRouter } from "expo-router";
 
 export default function Layout() {
-  const router = useRouter();
+	const router = useRouter();
 
-  const theme = useTheme();
+	const theme = useTheme();
 
-  return (
-    <Stack
-      screenOptions={{
-        tabBarActiveTintColor: theme.colors.primary,
-        tabBarStyle: {
-          backgroundColor: theme.colors.background,
-        },
-        tabBarLabelStyle: {
-          fontFamily: "PPPierSans-Regular",
-          fontSize: 12,
-        },
-      }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{
-          headerLeft: () => (
-            <IonIcon
-              name="arrow-back"
-              size={30}
-              color={theme.colors.text}
-              onPress={() => router.back()}
-              style={{ marginLeft: 10 }}
-            />
-          ),
-          headerStyle: {
-            backgroundColor: "#9393BA",
-          },
-          headerTitle: "Create a New Galaxy",
-        }}
-      />
+	return (
+		<Stack
+			screenOptions={{
+				tabBarActiveTintColor: theme.colors.primary,
+				tabBarStyle: {
+					backgroundColor: theme.colors.background,
+				},
+				tabBarLabelStyle: {
+					fontFamily: "PPPierSans-Regular",
+					fontSize: 12,
+				},
+			}}
+		>
+			<Stack.Screen
+				name="index"
+				options={{
+					headerLeft: () => (
+						<IonIcon
+							name="arrow-back"
+							size={30}
+							color={theme.colors.text}
+							onPress={() => router.back()}
+							style={{ marginLeft: 10 }}
+						/>
+					),
+					headerStyle: {
+						backgroundColor: "#9393BA",
+					},
+					headerTitle: "Create a New Galaxy",
+				}}
+			/>
 
-      <Stack.Screen
-        name="addFriends"
-        options={{
-          headerShown: true, // Show header for this screen
-          headerStyle: {
-            backgroundColor: "#9393BA",
-          },
-          headerTitle: "Add friends!",
+			<Stack.Screen
+				name="addFriends"
+				options={{
+					headerShown: true, // Show header for this screen
+					headerStyle: {
+						backgroundColor: "#9393BA",
+					},
+					headerTitle: "Add Friends",
 
-          headerLeft: () => (
-            <IonIcon
-              name="arrow-back"
-              size={30}
-              color={theme.colors.text}
-              onPress={() => router.back()}
-              style={{ marginLeft: 10 }}
-            />
-          ),
-        }}
-      />
-    </Stack>
-  );
+					headerLeft: () => (
+						<IonIcon
+							name="arrow-back"
+							size={30}
+							color={theme.colors.text}
+							onPress={() => router.back()}
+							style={{ marginLeft: 10 }}
+						/>
+					),
+				}}
+			/>
+		</Stack>
+	);
 }
--- app/tabs/galaxy/newGalaxy/addFriends.js
+++ app/tabs/galaxy/newGalaxy/addFriends.js
@@ -1,10 +1,10 @@
 import { useEffect, useState, useContext } from "react";
 import {
-  View,
-  Text,
-  StyleSheet,
-  FlatList,
-  ImageBackground,
+	View,
+	Text,
+	StyleSheet,
+	FlatList,
+	ImageBackground,
 } from "react-native";
 import { useRouter } from "expo-router";
 import { useLocalSearchParams } from "expo-router";
@@ -15,117 +15,115 @@ import FriendComponent from "@/components/FriendComponent";
 import spaceBackgroundImage from "@/assets/space-bg.jpg";
 
 export default function AddFriends() {
-  const router = useRouter();
-  const { galaxyName } = useLocalSearchParams();
-  const { userId } = useContext(UserContext);
-  const [friendIds, setFriendIds] = useState([]);
-  const [friendPlanets, setFriendPlanets] = useState([]);
+	const router = useRouter();
+	const { galaxyName } = useLocalSearchParams();
+	const { userId } = useContext(UserContext);
+	const [friendIds, setFriendIds] = useState([]);
+	const [friendPlanets, setFriendPlanets] = useState([]);
 
-  const fetchFriendIds = async () => {
-    const friendsIds = await fetchFriends(userId);
-    console.log("Fetched Friend IDs:", friendsIds);
-    setFriendIds(friendsIds);
-  };
-  const fetchPlanets = async () => {
-    try {
-      const allPlanets = await fetchAllPlanets();
-      console.log("All Planets:", allPlanets);
+	const fetchFriendIds = async () => {
+		const friendsIds = await fetchFriends(userId);
+		setFriendIds(friendsIds);
+	};
 
-      let array = [];
-      for (let i = 0; i < friendIds.length; i++) {
-        const friendId = friendIds[i];
-        const friendPlanet = allPlanets.find(
-          (planet) => planet.user_id === friendId
-        );
-        if (friendPlanet) {
-          array.push(friendPlanet);
-        }
-      }
+	const fetchPlanets = async () => {
+		try {
+			const allPlanets = await fetchAllPlanets();
 
-      console.log("Array of Friend Planets:", array);
-      setFriendPlanets(array);
-    } catch (error) {
-      console.error("Error fetching planets:", error);
-    }
-  };
+			let array = [];
+			for (let i = 0; i < friendIds.length; i++) {
+				const friendId = friendIds[i];
+				const friendPlanet = allPlanets.find(
+					(planet) => planet.user_id === friendId
+				);
+				if (friendPlanet) {
+					array.push(friendPlanet);
+				}
+			}
 
-  useEffect(() => {
-    fetchFriendIds();
-  }, []);
+			setFriendPlanets(array);
+		} catch (error) {
+			console.error("Error fetching planets:", error);
+		}
+	};
 
-  useEffect(() => {
-    if (friendIds.length > 0) {
-      fetchPlanets(); // fetch planets when friendIds is updated
-    }
-  }, [friendIds]);
-  return (
-    <ImageBackground
-      source={spaceBackgroundImage}
-      resizeMode="cover"
-      style={styles.image}
-    >
-      <View style={styles.container}>
-        <View style={styles.topContainer}>
-          <Text style={styles.topText}>Add friends to</Text>
-          <Text style={styles.text}>{galaxyName}</Text>
-        </View>
-        <FlatList
-          data={friendPlanets}
-          renderItem={({ item }) => (
-            <FriendComponent
-              galaxyName={galaxyName}
-              planetObj={item}
-              // userId={item.user_id}
-              // username={item.username}
-              // firstname={item.first_name}
-              // lastname={item.last_name}
-            />
-          )}
-        ></FlatList>
-        <View style={styles.bottomContainer}>
-          <Text style={styles.bottomText}>
-            "Don't see your friend? Add them from Contacts or invite them to
-            join Planet!
-          </Text>
-        </View>
-      </View>
-    </ImageBackground>
-  );
+	useEffect(() => {
+		fetchFriendIds();
+	}, []);
+
+	useEffect(() => {
+		if (friendIds.length > 0) {
+			fetchPlanets(); // fetch planets when friendIds is updated
+		}
+	}, [friendIds]);
+	return (
+		<ImageBackground
+			source={spaceBackgroundImage}
+			resizeMode="cover"
+			style={styles.image}
+		>
+			<View style={styles.container}>
+				<View style={styles.topContainer}>
+					<Text style={styles.topText}>Add friends to</Text>
+					<Text style={styles.text}>{galaxyName}</Text>
+				</View>
+				<FlatList
+					data={friendPlanets}
+					renderItem={({ item }) => (
+						<FriendComponent
+							galaxyName={galaxyName}
+							planetObj={item}
+							// userId={item.user_id}
+							// username={item.username}
+							// firstname={item.first_name}
+							// lastname={item.last_name}
+						/>
+					)}
+				></FlatList>
+				<View style={styles.bottomContainer}>
+					<Text style={styles.bottomText}>
+						"Don't see your friend? Add them from Contacts or invite
+						them to join Planet!
+					</Text>
+				</View>
+			</View>
+		</ImageBackground>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    // backgroundColor: "rgb(29, 27, 30)",
-  },
-  image: {
-    flex: 1,
-  },
-  text: {
-    color: "white",
-    fontSize: 35,
-    fontFamily: "PPPierSans-Regular",
-  },
-  topText: {
-    color: "white",
-    fontSize: 18,
-    fontFamily: "PPPierSans-Regular",
-  },
-  bottomContainer: {
-    padding: 10,
-    marginLeft: 20,
-    marginRight: 20,
-  },
-  topContainer: {
-    marginTop: 10,
-    padding: 10,
-    alignItems: "center",
-  },
-  bottomText: {
-    color: "white",
-    fontSize: 13,
-    fontStyle: "italic",
-  },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		// backgroundColor: "rgb(29, 27, 30)",
+	},
+	image: {
+		flex: 1,
+	},
+	text: {
+		color: "white",
+		fontSize: 35,
+		fontFamily: "PPPierSans-Regular",
+	},
+	topText: {
+		color: "white",
+		fontSize: 18,
+		fontFamily: "PPPierSans-Regular",
+	},
+	bottomContainer: {
+		padding: 10,
+		marginLeft: 20,
+		marginRight: 20,
+	},
+	topContainer: {
+		marginTop: 10,
+		padding: 10,
+		alignItems: "center",
+	},
+	bottomText: {
+		color: "white",
+		fontSize: 13,
+		fontStyle: "italic",
+	},
 });
