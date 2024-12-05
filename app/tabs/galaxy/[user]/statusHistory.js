237d3be97d002c1abf5ad898b105ea956136ef1a
LWCoding <DiamondMob01@gmail.com>
2024-12-04 20:03:43 -0800

    Emotions show correctly in post history, post history is now sorted by date

--- app/tabs/galaxy/[user]/statusHistory.js
+++ app/tabs/galaxy/[user]/statusHistory.js
@@ -4,102 +4,114 @@ import { useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
 import { useTheme } from "react-native-paper";
 import {
-  fetchAllStatusesById,
-  findPlanetById,
-  findStatusById,
+	fetchAllStatusesById,
+	findPlanetById,
+	findStatusById,
 } from "@/database/db";
 
 // Import components
 import StatusHistoryBlock from "@/components/StatusHistoryBlock";
 
 export default function statusHistory() {
-  const { user: inputtedUserId } = useLocalSearchParams(); // Get the user's info from navigation
+	const { user: inputtedUserId } = useLocalSearchParams(); // Get the user's info from navigation
 
-  const theme = useTheme();
-  const [user, setUser] = useState(null);
-  const [statuses, setStatuses] = useState(null);
+	const theme = useTheme();
+	const [user, setUser] = useState(null);
+	const [statuses, setStatuses] = useState(null);
 
-  const fetchUserInfo = async () => {
-    const user = await findPlanetById(inputtedUserId);
-    const statuses = await fetchAllStatusesById(user.user_id);
+	const fetchUserInfo = async () => {
+		const user = await findPlanetById(inputtedUserId);
+		const statuses = await fetchAllStatusesById(user.user_id);
 
-    setUser(user);
-    setStatuses(statuses);
-  };
+		setUser(user);
 
-  useEffect(() => {
-    fetchUserInfo();
-  }, []);
+		// Sort the statuses based on date (latest first)
+		const statusesSorted = statuses.sort((a, b) => {
+			return new Date(b.created_at) - new Date(a.created_at);
+		});
 
-  if (!user || !statuses) {
-    return (
-      <View
-        style={[styles.container, { backgroundColor: theme.colors.background }]}
-      >
-        <View style={styles.activityIndicatorContainer}>
-          <ActivityIndicator size="large" animating={true} />
-        </View>
-      </View>
-    );
-  }
+		setStatuses(statusesSorted);
+	};
 
-  return (
-    <View
-      style={[styles.container, { backgroundColor: theme.colors.background }]}
-    >
-      <Text style={styles.text}>{user.first_name}'s Post History</Text>
-      <FlatList
-        style={styles.statusesList}
-        data={statuses}
-        contentContainerStyle={{ alignItems: "center" }}
-        keyExtractor={(item) => item.status_id.toString()}
-        renderItem={({ item }) => (
-          <StatusHistoryBlock status={item} user={user} />
-        )}
-      />
-    </View>
-  );
+	useEffect(() => {
+		fetchUserInfo();
+	}, []);
+
+	if (!user || !statuses) {
+		return (
+			<View
+				style={[
+					styles.container,
+					{ backgroundColor: theme.colors.background },
+				]}
+			>
+				<View style={styles.activityIndicatorContainer}>
+					<ActivityIndicator size="large" animating={true} />
+				</View>
+			</View>
+		);
+	}
+
+	return (
+		<View
+			style={[
+				styles.container,
+				{ backgroundColor: theme.colors.background },
+			]}
+		>
+			<Text style={styles.text}>{user.first_name}'s Post History</Text>
+			<FlatList
+				style={styles.statusesList}
+				data={statuses}
+				contentContainerStyle={{ alignItems: "center" }}
+				keyExtractor={(item) => item.status_id.toString()}
+				renderItem={({ item }) => (
+					<StatusHistoryBlock status={item} user={user} />
+				)}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    // justifyContent: "center",
-    position: "relative",
-  },
-  imageLoadingIndicator: {
-    position: "absolute",
-    top: "50%", // Position loading indicator over picture
-  },
-  activityIndicatorContainer: {
-    alignItems: "center",
-    justifyContent: "center",
-    flex: 1,
-  },
-  text: {
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 30,
-    margin: 30,
-    fontFamily: "PPPierSans-Regular",
-  },
-  statusText: {
-    color: "black",
-    // fontWeight: "bold",
-    fontFamily: "PPPierSans-Regular",
-    fontSize: 15,
-  },
-  statusesList: {
-    width: "100%",
-  },
-  statusContainer: {
-    margin: 3,
-    width: "80%",
-    backgroundColor: "#CBCBE2",
-    borderRadius: 15,
-    padding: 15,
-    alignItems: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		// justifyContent: "center",
+		position: "relative",
+	},
+	imageLoadingIndicator: {
+		position: "absolute",
+		top: "50%", // Position loading indicator over picture
+	},
+	activityIndicatorContainer: {
+		alignItems: "center",
+		justifyContent: "center",
+		flex: 1,
+	},
+	text: {
+		color: "white",
+		fontWeight: "bold",
+		fontSize: 30,
+		margin: 30,
+		fontFamily: "PPPierSans-Regular",
+	},
+	statusText: {
+		color: "black",
+		// fontWeight: "bold",
+		fontFamily: "PPPierSans-Regular",
+		fontSize: 15,
+	},
+	statusesList: {
+		width: "100%",
+	},
+	statusContainer: {
+		margin: 3,
+		width: "80%",
+		backgroundColor: "#CBCBE2",
+		borderRadius: 15,
+		padding: 15,
+		alignItems: "center",
+	},
 });
--- components/StatusHistoryBlock.js
+++ components/StatusHistoryBlock.js
@@ -6,61 +6,72 @@ import { useTheme } from "react-native-paper";
 import Planet from "./Planet";
 
 export default function ProfileButton({ user, status }) {
-  const theme = useTheme();
+	const theme = useTheme();
 
-  const formatTimestamp = (timestamp) => {
-    const date = new Date(timestamp);
+	const formatTimestamp = (timestamp) => {
+		const date = new Date(timestamp);
 
-    // Get month (add 1 to correct the zero-based index)
-    const month = date.getMonth() + 1; // Add 1 to month to make it human-readable
-    const day = date.getDate();
+		// Get month (add 1 to correct the zero-based index)
+		const month = date.getMonth() + 1; // Add 1 to month to make it human-readable
+		const day = date.getDate();
 
-    // Get hours and handle the 12-hour format
-    let hours = date.getHours();
-    const minutes = String(date.getMinutes()).padStart(2, "0"); // Ensure two-digit minute format
+		// Get hours and handle the 12-hour format
+		let hours = date.getHours();
+		const minutes = String(date.getMinutes()).padStart(2, "0"); // Ensure two-digit minute format
 
-    // Handle AM/PM
-    const ampm = hours >= 12 ? "pm" : "am";
-    hours = hours % 12;
-    hours = hours === 0 ? 12 : hours; // If hours is 0, set it to 12
+		// Handle AM/PM
+		const ampm = hours >= 12 ? "pm" : "am";
+		hours = hours % 12;
+		hours = hours === 0 ? 12 : hours; // If hours is 0, set it to 12
 
-    return `${month}/${day} at ${hours}:${minutes}${ampm}`;
-  };
+		return `${month}/${day} at ${hours}:${minutes}${ampm}`;
+	};
 
-  return (
-    <View style={[styles.container, { backgroundColor: theme.colors.primary }]}>
-      <Planet
-        isClickable={false}
-        isNameVisible={false}
-        width={50}
-        height={50}
-        userId={user.user_id}
-      />
-      <Text style={styles.timestamp}>{formatTimestamp(status.created_at)}</Text>
-      <Image style={styles.postImage} source={{ uri: status.image_url }} />
-      <Text style={styles.postText}>{status.status_text}</Text>
-    </View>
-  );
+	return (
+		<View
+			style={[
+				styles.container,
+				{ backgroundColor: theme.colors.primary },
+			]}
+		>
+			<Planet
+				isClickable={false}
+				isNameVisible={false}
+				width={50}
+				height={50}
+				hardCodedEmotion={status.emotion}
+			/>
+			<Text style={styles.timestamp}>
+				{formatTimestamp(status.created_at)}
+			</Text>
+			<Image
+				style={styles.postImage}
+				source={{ uri: status.image_url }}
+			/>
+			<Text style={styles.postText}>{status.status_text}</Text>
+		</View>
+	);
 }
 
 const styles = {
-  container: {
-    width: "95%",
-    height: 80,
-    margin: 5,
-    borderRadius: "5%",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    fontFamily: "PPPierSans-Regular",
-  },
-  postImage: {
-    flex: 1,
-  },
-  postText: {
-    textAlign: "center",
-    flex: 3,
-    paddingHorizontal: 5,
-    fontFamily: "PPPierSans-Regular",
-  },
+	container: {
+		width: "95%",
+		height: 80,
+		margin: 5,
+		padding: 10,
+		borderRadius: "5%",
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		fontFamily: "PPPierSans-Regular",
+	},
+	postImage: {
+		flex: 1,
+	},
+	postText: {
+		textAlign: "center",
+		flex: 3,
+		paddingHorizontal: 5,
+		fontFamily: "PPPierSans-Regular",
+	},
 };
